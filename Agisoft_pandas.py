fbdd405d622cff2bf29b47ab49e5c7dd3423b8f2
tvanderweide <thomasvanderweide@u.boisestate.edu>
2020-10-25 12:26:38 -0600

    Added a check for csv library

--- Agisoft_pandas.py
+++ Agisoft_pandas.py
@@ -15,15 +15,33 @@ except ImportError:
 # import split_in_chunks_python
 
 try:
-    import pandas as pd
-    imgCount_df = pd.DataFrame({"Date": 0,
-               "Medium" : 0,
-               "MedPts": 0,
-               "MedTie": 0,
-               "MedptsTie": 0,
-               "MedExtra": 0,}, index=[0])
+    # import pandas as pd
+    # imgCount_df = pd.DataFrame({"Date": 0,
+    #            "Medium" : 0,
+    #            "MedPts": 0,
+    #            "MedTie": 0,
+    #            "MedptsTie": 0,
+    #            "MedExtra": 0,}, index=[0])
     
-    print("Pandas has been installed")
+    # print("Pandas has been installed")
+    import csv
+        mainDict = {"Date": 0,
+                "Accuracy" : 0,
+                "Key_Limit" : 0,
+                "Tie_Limit" : 0,
+                "Quality" : 0,
+                "Filter" : 0,
+                "Aligned" : 0,
+                "Total" : 0,
+                "Img_Quality_Avg" : 0,
+                "RU_Thresh" : 0,
+                "PA_Thresh" : 0,
+                "RE_Thresh" : 0,
+                "Tie_ptsBefore" : 0,
+                "Marker_errBefore" : 0,
+                "Tie_ptsAfter" : 0,
+                "Marker_errAfter" : 0}
+    print("CSV can successfully be called")
 except:
-    print("Failed to import Pandas")
+    print("Failed to use CSV library")
 
